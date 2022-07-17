048ea2a8dd284d3bf10440bf611d9395028d3e8b
VISHAL AGARWAL <93400613+bicku1995@users.noreply.github.com>
2022-07-17 23:40:25 +0530

    Add files via upload
    Assignment submitted

---
+++ FrontEndAssignment-MoviesWorld/README.md
@@ -0,0 +1,4 @@
+# FrontEndGradedAssignmentReact3-9_movies-on-the-tip
+json data
+img files used as provided
+env file with the contents(REACT_APP_API_BASE_URL=http://localhost:4000)
---
+++ FrontEndAssignment-MoviesWorld/src/components/NoDataMatched.tsx
@@ -0,0 +1,15 @@
+const NoDataMatched = () => {
+    return (
+        <main>
+            <section>
+                <header>
+                    <h1>404 NOT FOUND</h1>
+                </header>
+                <hr />
+                <img alt="Resource Not Found"/>
+            </section>
+        </main>
+    );
+};
+
+export default NoDataMatched;
